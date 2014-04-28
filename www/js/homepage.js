8542dd10327a3bc154375387856b056ae9c85fb7
justdoit <justdoit920823@gmail.com>
2014-04-28 19:37:27 +0800

    add follow and unfollow click action handler

--- www/js/homepage.js
+++ www/js/homepage.js
@@ -1,3 +1,26 @@
+
+// support follow actions
+var supactions={follow:"follow",unfollow:"unfollow"};
+
 $(document).ready(function(){
-   
+   $(".ue-people-action").click(function(){
+        var settings={
+              url:"/user/action/follow",
+              type:"post",
+              dataType:"json",
+              data:{action:supactions[$(this).attr("id")],_xsrf:$("input[name='_xsrf']").val(),flwid:$(this).attr("followid")},
+              error:function(jq,data,errorThrow){
+                     alert(data);
+                   },
+              success:function(result){
+                        if(result.status){
+                            alert(result.msg);
+                        }
+                        else{
+                            alert(result.msg);
+                        }
+                      }
+            };
+        $.ajax(settings);
+    });
 });
